d9502bb9373f2dbb4ee4cc9058ecb08f041f9838
MbuuseBen <teezymak@gmail.com>
2021-08-11 10:35:33 +0300

    changed layout

---
+++ lib/screens/ProductDetails/product_details.dart
@@ -0,0 +1,213 @@
+ import 'package:flutter/material.dart';
+import 'package:flutter_elegant_number_button/flutter_elegant_number_button.dart';
+
+// class ProductDetails extends StatelessWidget {
+//   const ProductDetails({
+//     Key? key,
+//   }) : super(key: key);
+
+ class ProductDetails extends StatefulWidget {
+   const ProductDetails({Key? key}) : super(key: key);
+
+   @override
+   _ShopPageState createState() => _ShopPageState();
+ }
+
+ class _ShopPageState extends State<ProductDetails> {
+   num count = 0;
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20.0),
+          decoration: BoxDecoration(
+              color: Colors.grey.shade100,
+              borderRadius: BorderRadius.circular(10.0)),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+            children: <Widget>[
+              SizedBox(height: 5.0),
+              Column(
+                children: [
+                  Container(
+                    width: 400,
+                    child: Card(
+                      margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+                      elevation: 10,
+                      child:Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            SizedBox(height: 1.0),
+                             AspectRatio(
+                              aspectRatio: 4 / 2,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10.0),
+                                child: Image.network(
+                                  'https://cdn.dribbble.com/users/4861236/screenshots/14279740/automobile___car_promotion_poster_design__orange_.jpg',
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+
+                  ),
+                  SizedBox(height: 15.0),
+                  Container(
+                    width: 400,
+                    child: Card(
+                      margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+                      elevation: 10,
+                      child:Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              SizedBox(height: 5.0),
+                              Text('UGX 100,000',style: TextStyle(fontSize: 25)),
+                              SizedBox(height: 5.0),
+                              Text('Ignition Coil',),
+                              SizedBox(height: 5.0),
+                              // FlatButton(
+                              //   splashColor: Colors.red,
+                              //   color: Colors.amber,
+                              //   textColor: Colors.white,
+                              //   child: Text('Add To Cart',),
+                              //   onPressed: () {},
+                              // ),
+
+                              Container(
+                                margin: const EdgeInsets.all(15.0),
+                                child: Row(
+                                  children: [
+                                    ElegantNumberButton(
+                                      initialValue: count,
+                                      buttonSizeWidth: 40,
+                                      buttonSizeHeight: 35,
+                                      color: Colors.amber,
+                                      minValue: 0,
+                                      maxValue: 100,
+                                      step: 1,
+                                      decimalPlaces: 0,
+                                      onChanged: (value) { // get the latest value from here
+                                        setState(() {
+                                          count = value;
+                                        });
+                                      },
+                                    ),
+                                    Spacer(),
+                                    FlatButton(
+                                      splashColor: Colors.red,
+                                        color: Colors.amber,
+                                        textColor: Colors.white,
+                                        child: Text('Add To Cart',),
+                                        onPressed: () {},
+                                    ),
+                                  ],
+                                ),
+                                // margin: EdgeInsets.all(20),
+                                // child: FlatButton(
+                                //   splashColor: Colors.red,
+                                //     color: Colors.amber,
+                                //     textColor: Colors.white,
+                                //     child: Text('Add To Cart',),
+                                //     onPressed: () {},
+                                // ),
+                              ),
+
+                            ],
+                        ),
+
+                      ),
+                    ),
+
+                  ),
+                  SizedBox(height: 5.03),
+                  Container(
+                    margin: const EdgeInsets.all(15.0),
+                    child: Row(
+                      children: const [
+                        SizedBox(width: 5.0),
+                        Text('PRODUCT DETAILS'),
+                      ],
+                    ),
+                  ),
+
+                  Container(
+                    width: 400,
+                    child: Card(
+                      margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+                      elevation: 10,
+                      child:Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            SizedBox(width: 5.0),
+                            Text('Used Spare Parts,Vehicle Fitment	Brand and Year Specific Braking System Component. The braking system of a vehicle is a complex series of components that work together to stop its motion temporarily while driving or to maintain a steady position while parked.',style: TextStyle(fontSize: 17)),
+                            SizedBox(width: 5.0),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 5.03),
+                  Container(
+                    margin: const EdgeInsets.all(15.0),
+                    child: Row(
+                      children: const [
+                        SizedBox(width: 5.0),
+                        Text('CHECK OUT OTHER PRODUCTS'),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 300,
+                    child: Card(
+                      margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+                      elevation: 10,
+                      child:Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: ListView.builder(
+                            itemCount: 5,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Container(
+                                margin: const EdgeInsets.all(10.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Image.network(
+                                      'https://cdn3.louis.de/dynamic/articles/o_resize,w_1800,h_1800,m_limit,c_fff/dd.76.d7.10037628Motul71004T15W501l970DET0118.JPG',
+                                      width: 100,
+                                    ),
+                                    SizedBox(height: 10.0),
+                                    Text('Engine Oil'),
+                                    SizedBox(height: 5.0),
+                                    Text('UGX 10,000'),
+                                  ],
+                                ),
+                              );
+                            }),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ]),
+      ),
+    );
+  }
+}
--- lib/screens/Shop/shop_page.dart
+++ lib/screens/Shop/shop_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class ShopPage extends StatefulWidget {
@@ -8,6 +10,116 @@ class ShopPage extends StatefulWidget {
 }
 
 class _ShopPageState extends State<ShopPage> {
+  final _vehicles = [
+    {'name': 'BMW', 'logo': 'https://www.carlogos.org/car-logos/bmw-logo.png'},
+    {
+      'name': 'Ford',
+      'logo': 'https://www.carlogos.org/car-logos/ford-logo.png'
+    },
+    {
+      'name': 'Honda',
+      'logo': 'https://www.carlogos.org/car-logos/honda-logo.png'
+    },
+    {
+      'name': 'Subaru',
+      'logo': 'https://www.carlogos.org/car-logos/subaru-logo.png'
+    },
+    {
+      'name': 'Toyota',
+      'logo': 'https://www.carlogos.org/car-logos/hyundai-logo.png'
+    },
+    {
+      'name': 'Hyundai',
+      'logo': 'https://www.carlogos.org/car-logos/hyundai-logo.png'
+    },
+    {
+      'name': 'Lexus',
+      'logo': 'https://www.carlogos.org/car-logos/lexus-logo.png'
+    },
+    {
+      'name': 'Benz',
+      'logo': 'https://www.carlogos.org/car-logos/mercedes-benz-logo.png'
+    },
+    {
+      'name': 'Mitsubishu',
+      'logo': 'https://www.carlogos.org/car-logos/mitsubishi-logo.png'
+    },
+  ];
+
+  final _services = [
+    {
+      'name': 'Lubricant',
+      'logo': 'https://www.carlogos.org/car-logos/bmw-logo.png'
+    },
+    {
+      'name': 'Air condtion',
+      'logo': 'https://www.carlogos.org/car-logos/ford-logo.png'
+    },
+    {
+      'name': 'Engine',
+      'logo': 'https://www.carlogos.org/car-logos/honda-logo.png'
+    },
+    {
+      'name': 'Brake Fluid',
+      'logo': 'https://www.carlogos.org/car-logos/subaru-logo.png'
+    },
+    {
+      'name': 'Wiper',
+      'logo': 'https://www.carlogos.org/car-logos/hyundai-logo.png'
+    },
+    {
+      'name': 'Braking System',
+      'logo': 'https://www.carlogos.org/car-logos/hyundai-logo.png'
+    },
+    {
+      'name': 'Wheels',
+      'logo': 'https://www.carlogos.org/car-logos/mercedes-benz-logo.png'
+    },
+    {
+      'name': 'Air Intake System',
+      'logo': 'https://www.carlogos.org/car-logos/lexus-logo.png'
+    },
+    {
+      'name': 'Hydraulic',
+      'logo': 'https://www.carlogos.org/car-logos/mitsubishi-logo.png'
+    },
+    {
+      'name': 'Cooler',
+      'logo': 'https://www.carlogos.org/car-logos/mitsubishi-logo.png'
+    },
+  ];
+
+  final _hotDeals = [
+    {
+      'name': 'Engine Oil',
+      'logo':
+      'https://cdn3.louis.de/dynamic/articles/o_resize,w_1800,h_1800,m_limit,c_fff/dd.76.d7.10037628Motul71004T15W501l970DET0118.JPG'
+    },
+    {
+      'name': 'Tires',
+      'logo': 'https://www.pngall.com/wp-content/uploads/8/Tire-PNG-Pic.png'
+    },
+    {
+      'name': 'Steering wheel',
+      'logo':
+      'https://pngimg.com/uploads/steering_wheel/steering_wheel_PNG16691.png'
+    },
+    {
+      'name': 'Tail Lights',
+      'logo':
+      'https://i.pinimg.com/originals/fc/c4/80/fcc480e0879b067a4687908836d6fe90.png'
+    },
+    {
+      'name': 'Rims',
+      'logo':
+      'https://www.pngitem.com/pimgs/m/4-45309_car-wheel-png-ruff-rims-transparent-png.png'
+    },
+    {
+      'name': 'Leather Seats',
+      'logo':
+      'https://www.pngkit.com/png/full/389-3892110_leather-trimmed-interiors-nw-running-boards-car.png'
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,43 +147,69 @@ class _ShopPageState extends State<ShopPage> {
       body: SingleChildScrollView(
         child: Column(children: [
           Container(
-            height: 300,
-            child: Image.network(
-                'https://cdn.dribbble.com/users/4861236/screenshots/14279740/automobile___car_promotion_poster_design__orange_.jpg'),
-          ),
-
-
-          Container(
             width: 400,
             child: Card(
               margin: const EdgeInsets.symmetric(horizontal: 5.0),
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(3.0)),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
               elevation: 10,
               child:Padding(
                 padding: const EdgeInsets.all(5.0),
-                child: Wrap(
-            children: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-                .map(
-                  (e) => Container(
-                margin: const EdgeInsets.all(10.0),
-                child: Column(children: [
-                  Container(
-                    padding: const EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                        color: Colors.amber,
-                        borderRadius: BorderRadius.circular(50.0)),
-                    child: Icon(Icons.car_rental),
-                  ),
-                  SizedBox(height: 5.0),
-                  Text('Engine'),
-                ]),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    SizedBox(height: 1.0),
+                    AspectRatio(
+                      aspectRatio: 4 / 2,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0),
+                        child: Image.network(
+                          'https://cdn.dribbble.com/users/4861236/screenshots/14279740/automobile___car_promotion_poster_design__orange_.jpg',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            )
-                .toList(),
+            ),
+
           ),
-    ),
-    ),
-    ),
+          SizedBox(height: 15.0),
+       Wrap(
+            children: _services.map((_data) {
+              final _color = Color((Random().nextDouble() * 0xFFFFFF).toInt());
+              return FractionallySizedBox(
+                widthFactor: 0.19,
+                child: Container(
+                  margin: const EdgeInsets.all(10.0),
+                  child: Column(children: [
+                    Container(
+                      padding: const EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                          gradient: RadialGradient(
+                            center: const Alignment(-0.8, -0.6),
+                            colors: [
+                              _color.withOpacity(0.5),
+                              _color.withOpacity(0.9),
+                            ],
+                            radius: 1.0,
+                          ),
+                          borderRadius: BorderRadius.circular(50.0)),
+                      child: const Icon(
+                        Icons.car_rental,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 5.0),
+                    Text(
+                      _data['name']!,
+                      textAlign: TextAlign.center,
+                    ),
+                  ]),
+                ),
+              );
+            }).toList(),
+          ),
           Container(
             margin: const EdgeInsets.all(15.0),
             child: Row(
@@ -94,21 +232,29 @@ class _ShopPageState extends State<ShopPage> {
                 elevation: 10,
                 child:Padding(
                   padding: const EdgeInsets.all(5.0),
-              child: Wrap(
-                children: [1, 2, 3, 4, 5, 6, 7, 8, 9]
-                    .map(
-                      (e) => Container(
-                    margin: const EdgeInsets.all(15.0),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(Icons.car_rental),
-                        SizedBox(width: 5.0),
-                        Text('Car name'),
-                      ],
-                    ),
-                  ),
-                )
+                  child: Wrap(
+                    children: _vehicles
+                        .map(
+                          (_data) => FractionallySizedBox(
+                        widthFactor: 0.3,
+                        child: Container(
+                          margin: const EdgeInsets.symmetric(vertical: 15.0),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Image.network(_data["logo"]!, width: 40),
+                              const SizedBox(width: 5.0),
+                              Flexible(
+                                child: Text(
+                                  _data["name"]!,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
                     .toList(),
               ),
             ),
